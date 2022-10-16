a6757cddb68d9551f4848cf4d82d922590b5aa29
Alan Ricardo <alan.ricardo.silva@hotmail.com>
2022-10-16 17:37:39 -0300

    ci: add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,9 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) =>{
+    if(configType === 'PRODUCTION'){
+      config.base = '/ignite-lab-03'
   }
 }
